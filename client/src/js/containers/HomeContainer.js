86560c9b555f1cc1e3d155dd84f506aa48d042fd
Mduduzi <ndhlovu@pop-os.localdomain>
2020-02-25 15:09:27 +0000

    remove unused files

--- client/src/js/containers/HomeContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class HomeContainer extends Component {
-  render() {
-    return <div>React boiler plate</div>;
-  }
-}
-
-export default HomeContainer;
--- client/src/js/selectors/alertSelectors.js
+++
@@ -1,1 +0,0 @@
-export const getAlert = state => state.alert;
